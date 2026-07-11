22942ff503db5da1dc58393d8ce2cf65cdfad2cc
Rebel OOTD <rebelootd@gmail.com>
2026-07-11 22:02:02 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const daftarProduk = document.getElementById("produk");
+
+async function loadProduk() {
+  const snapshot = await getDocs(collection(db, "products"));
+
+  daftarProduk.innerHTML = "";
+
+  snapshot.forEach((doc) => {
+    const p = doc.data();
+
+    daftarProduk.innerHTML += `
+      <div class="card">
+        <img src="${p.image}" width="250">
+        <h3>${p.Name}</h3>
+        <p>⭐ ${p.rating}</p>
+        <p>💰 ${p.price}</p>
+        <a href="${p.Link}" target="_blank">
+          <button>🛒 Shopee</button>
+        </a>
+      </div>
+    `;
+  });
+}
+
+loadProduk();
