1f91f58c6cc5e1b5bb60461398235fd3276af15a
Rebel OOTD <rebelootd@gmail.com>
2026-07-11 22:10:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,25 +4,35 @@ import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.6.0/f
 const daftarProduk = document.getElementById("produk");
 
 async function loadProduk() {
-  const snapshot = await getDocs(collection(db, "products"));
+
+  const snapshot = await getDocs(collection(db, "produk"));
 
   daftarProduk.innerHTML = "";
 
   snapshot.forEach((doc) => {
+
     const p = doc.data();
 
     daftarProduk.innerHTML += `
       <div class="card">
+
         <img src="${p.image}" width="250">
+
         <h3>${p.Name}</h3>
+
         <p>⭐ ${p.rating}</p>
-        <p>💰 ${p.price}</p>
+
+        <p>💰 Rp${p.price}</p>
+
         <a href="${p.Link}" target="_blank">
           <button>🛒 Shopee</button>
         </a>
+
       </div>
     `;
+
   });
+
 }
 
 loadProduk();
