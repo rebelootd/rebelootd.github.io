f169ffb7f1290abe8c88261ed79b52d0425804ae
Rebel OOTD <rebelootd@gmail.com>
2026-07-12 09:55:20 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -12,17 +12,39 @@ btn.addEventListener("click", async () => {
   const nama = document.getElementById("nama").value;
   const harga = document.getElementById("harga").value;
   const rating = document.getElementById("rating").value;
-  const gambar = document.getElementById("gambar").value;
   const link = document.getElementById("link").value;
 
-  await addDoc(collection(db, "products"), {
+  // Ambil file gambar
+  const file = document.getElementById("gambar").files[0];
 
+  if (!file) {
+    alert("Pilih gambar dulu ya 😊");
+    return;
+  }
+
+  // Upload ke Cloudinary
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "rebelootd_upload");
+
+  const upload = await fetch(
+    "https://api.cloudinary.com/v1_1/lbjs42j0/image/upload",
+    {
+      method: "POST",
+      body: formData
+    }
+  );
+
+  const hasil = await upload.json();
+
+  const gambar = hasil.secure_url;
+
+  await addDoc(collection(db, "products"), {
     Name: nama,
-    price: harga,
+    price: Number(harga),
     rating: Number(rating),
     image: gambar,
     Link: link
-
   });
 
   alert("Produk berhasil ditambahkan! 🎉");
